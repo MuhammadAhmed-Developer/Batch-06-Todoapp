df67bf33af71acba80c5c5517dacf3a4331aa961
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-30 00:08:19 -0700

    todo app

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import './App.css';
 
 function App() {
 
-  // const [inputValue, setinputValue] = useState()
+  const [inputValue, setinputValue] = useState('')
+  const [output, setOutput] = useState([])
 
-  const handleChange = () =>{
-
+  const handleChange = (event) =>{
+       setinputValue(event.target.value)
   }
 
   const handleSubmit = () =>{
+    if(inputValue.trim() !== ''){       
+      setOutput((itemlist)=> [...itemlist, inputValue]
+      )
+      setinputValue('')
+    }
+     console.log(inputValue)
+  }
 
+  const handleDelete = (index) =>{
+    setOutput((itemlist) => itemlist.filter((_, i) => i !== index));
   }
 
   return (
     <div className='main'>
       <div className="container">
         <h1>Todo App</h1>
-        <input type="text" value={input} onChange={handleChange} placeholder='Add Item' className='input' />
+        <input type="text" value={inputValue}  onChange={handleChange} placeholder='Add Item' className='input' />
         <br />
         <br />
         <button className='btn' onClick={handleSubmit}>Add+</button>
@@ -25,14 +35,18 @@ function App() {
 
           <h2 className='item'>Items</h2>
           <div className='output'>
-
-            {output.map((item, i)=>{
-                
-               <h2 key={i}>{item}</h2>
-            })}
-            <button className='btn-cross'> X </button>
+            
+            <div className='output'>
+           {output?.map((item, index)=>{
+            return <p key={index}>{item}
+            <button className='btn-cross' onClick={()=>handleDelete(index)}> X </button></p>
+            
+            
+                      
+          })}
 
             </div> 
+          </div>
 
         </div>
       </div>
