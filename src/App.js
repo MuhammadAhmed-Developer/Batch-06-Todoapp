52de6ce47858f4ecd07e371a2e5493315b7511f8
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-07-29 22:54:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // const [inputValue, setinputValue] = useState()
+
+  const handleChange = () =>{
+
+  }
+
+  const handleSubmit = () =>{
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className="container">
+        <h1>Todo App</h1>
+        <input type="text" value={input} onChange={handleChange} placeholder='Add Item' className='input' />
+        <br />
+        <br />
+        <button className='btn' onClick={handleSubmit}>Add+</button>
+        <div className='output-main'>
+
+          <h2 className='item'>Items</h2>
+          <div className='output'>
+
+            {output.map((item, i)=>{
+                
+               <h2 key={i}>{item}</h2>
+            })}
+            <button className='btn-cross'> X </button>
+
+            </div> 
+
+        </div>
+      </div>
+
     </div>
   );
 }
 
 export default App;
+
